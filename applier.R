5adf3f1bedc18a7662900a2b167f54c84f42a6d1
Daniel Paliura <vinstow@gmail.com>
2020-12-27 02:18:09 +0200

    Module completed

--- applier.R
+++ applier.R
@@ -2,9 +2,25 @@ source("holt.R", echo=FALSE)
 source("theil-wage.R", echo=FALSE)
 
 
-apply <- function(input){
-    res <- list()
-    
+apply.method <- function(input){
+    if (input$is.seasonal) {
+        res <- theil.wage(ts = input$ts,
+                          period = input$season.period,
+                          par1 = input$alpha,
+                          par2 = input$theta,
+                          par3 = input$beta,
+                          trend = input$is.trended)
+        res$method <- "Theil-Wage"
+        res$predict <- theil.wage.predict(res, 1:input$forecast.length)
+    }
+    else{
+        res <- holt(ts = input$ts,
+                    par1 = input$alpha,
+                    par2 = input$beta,
+                    trend = input$is.trended)
+        res$method <- "Holt"
+        res$predict <- holt.predict(res, 1:input$forecast.length)
+    }
     
     return(res)
 }
