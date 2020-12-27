230d3cffdbdbc0ca86921c89d9700c90ffad1c40
Daniel Paliura <vinstow@gmail.com>
2020-12-27 02:18:43 +0200

    module completed

--- interpreter.R
+++ interpreter.R
@@ -1,3 +1,64 @@
-interpret <- function(result){
-    print(result)
+interpret <- function(input, result){
+    tb <- "\t"
+    nl <- "\n"
+    sp <- " "
+    
+    ts.size <- length(input$ts)
+    pred.size <- input$forecast.length
+    
+    cat(paste0(tb, tb, "RESULT", nl,
+               tb, result$method, sp, "model",
+               nl,nl,
+               "Input:", nl,
+               "time-series of length", sp, ts.size, sp,
+               ifelse(input$is.trended, "with", "without"),
+               sp, "trend and", sp,
+               ifelse(input$is.seasonal, 
+                      paste0("with seasonal component of period", sp,
+                             input$season.period), 
+                      "without seasonal component"), nl,
+               "alpha =", sp, input$alpha, sp, "- smooth parameter", nl,
+               ifelse(input$is.trended,
+                      paste0("beta =", sp, input$beta, sp, 
+                             "- trend parameter", nl),
+                      ""),
+               ifelse(input$is.seasonal,
+                      paste0("theta =", sp, input$theta, sp, 
+                             "- seasonal parameter"),
+                      ""),
+               nl, nl))
+    
+    cat(paste0("Output:", nl,
+               "a =", sp, result$a, sp, "- smoothing component", nl,
+               "b =", sp, result$b, sp, "- trend component", nl,
+               ifelse(input$is.seasonal,
+                      paste0("theta (last period):", nl,
+                             paste(result$theta, collapse=", "),
+                             nl),
+                      ""),
+               "Predicted next", sp, pred.size, sp, "values:", nl,
+               paste0(result$predict, collapse=", "), nl,
+               "Max absolute error", sp,
+               max(abs(input$ts-result$control)), nl,
+               "See plot to view result visualisation",
+               nl, nl))
+    
+    
+    plot(y = c(input$ts, result$predict), 
+         x = 1:(ts.size + pred.size), 
+         type = "b", pch = 20,
+         col = c(rep(1, ts.size), rep(3, pred.size)),
+         xlab = "index", ylab = "value",
+         main = "Time series with predicted values")
+    
+    lines(y = c(result$control, result$predict[1]),
+          x = 1:(ts.size+1),
+          type = "b", pch = 20,
+          col = 2)
+    
+    legend.pos <- ifelse(result$b>0, "topleft", "topright")
+    legend(x = legend.pos,
+           legend = c("initial", "predicted", "control"),
+           pch = 20,
+           col = c(1, 3, 2))
 }
